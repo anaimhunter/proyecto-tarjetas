fa334b7c79d868d3c683279f45b019917c95681b
Ana M.Hunter <anaimhunter@hotmail.com>
2017-08-21 19:51:22 -0500

    Initial sharing of project

---
+++ Tarjeta.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of class Tarjeta here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Tarjeta
+{
+    private float Saldo;
+    
+    public Tarjeta()
+    {
+        Saldo=0;
+    }
+    
+    public void Deposita(float dinero)
+    {
+        Saldo= Saldo + dinero;
+    }
+    
+    public float Retira(float dinero)
+    {
+        if(Saldo>= dinero)
+        {
+            Saldo = Saldo - dinero;
+            return dinero;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    
+    public float Consultar()
+    {
+        return Saldo;
+    }
+}
